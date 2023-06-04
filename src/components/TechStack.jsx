b9ee9e826512cc403d10ee7df28f0757fa3cfb7d
Varad Sharma <117466176+Varad-Codes@users.noreply.github.com>
2023-06-04 12:44:55 +0530

    Update TechStack.jsx

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -7,10 +7,10 @@ const TechStack = () => {
             <ol className="font-bold cmd-output">
                 <li><span className="text-yellow">Languages</span><span className="text-cyan"> - JavaScript/TypeScript, Java, Python, C/C++, Rust</span></li>
                 <li><span className="text-yellow">Frontend</span><span className="text-cyan"> - React(Learning), JavaFX, Swing</span></li>
-                <li><span className="text-yellow">Backend</span><span className="text-cyan"> - NodeJS Express, SpringBoot</span></li>
+                <li><span className="text-yellow">Backend</span><span className="text-cyan"> - NodeJS Express, SpringBoot, rust</span></li>
                 <li><span className="text-yellow">DataBase</span><span className="text-cyan"> - MySQL, MongoDB</span></li>
                 <li className="text-cyan"><span className="text-yellow">Hardware</span> - RasberryPI, Arduino(Learning)</li>
-                <li><span className="text-yellow">Additional</span><span className="text-cyan"> - NextJS, Linux, VIM(I CAN EXIT IT!)</span></li>
+                <li><span className="text-yellow">Additional</span><span className="text-cyan"> - NextJS, Linux, tauri, VIM(I CAN EXIT IT!)</span></li>
             </ol>
             <br />
             <br />
