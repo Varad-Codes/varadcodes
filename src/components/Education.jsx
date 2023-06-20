76824025b0f1c345dc8b2a32efa38467a6b8471d
Varad Sharma <varad.code19@gamil.com>
2023-06-20 18:36:05 +0530

    school

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,7 +9,6 @@ const Education = () => {
                 <div>
                     <h1 className="cmd-output">
                         <p className="text-yellow font-bold">KG1 -{">"} Current(9th): <span className="text-cyan font-bold">Delhi Public School Kolar Road, Bhopal, Madhya Pradesh</span></p>
-                        <p className="text/* -yellow font-bold">Playschool: <span className="text-cyan font-bold">Lovedale Preschool Bhopal, Madhya Pradesh</span></p>
                     </h1>
                 </div>
             </div>
