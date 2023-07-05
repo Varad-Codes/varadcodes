f16dc59c834517b3378e444072a68c86f5cc3630
Varad Sharma <varad.code19@gmail.com>
2023-07-05 22:50:25 +0530

    new project!

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,6 +8,7 @@ const Projects = () => {
             <Prompt CMD={"Projects"}/>
             <div>
             <p className="cmd-output text-yellow">
+                <li>A Discord Bot, to help you get through though time, named LifeHelpElm!<span className="text-blue font-bold">(Private Bot, hit me on discord (fasterelm) if you want to try it!)</span></li>
                 <li>A coustom Minecraft 1.8.9 client <span className="text-blue font-bold">(Could not upload because of licence violation)</span></li>
                 <li>A Simple Encryption System written in C. <span className="text-blue"><a href="https://github.com/Varad-Sharma/SSE_ENCRYPTON" target="_blank" ><GitHub /></a></span></li>
                 <li>A Voice Chatbot for ATOM. A attempt on making a humanoid completely by the students with limited resources. <br /> Showcased at the DPSKolar annual function 2022. <a href="https://github.com/Varad-Codes/ATOM-bot" target="_blank" className="text-blue"><GitHub /></a></li>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -12,10 +12,10 @@ const Socials = () => {
                 <ol className="cmd-output font-bold">
                     <li className="text-yellow"><a href="https://github.com/Varad-Codes" target='_blank' rel="noreferrer"><GitHubIcon /><span className="text-blue"> (Current)</span></a></li>
                     <li className="text-yellow"><a href="https://github.com/Varad-Sharma"><GitHubIcon /><span className="text-blue"> (Old)</span></a></li>
-
                     <li className="text-yellow"><a href="https://instagram.com/f4ster444"> <InstagramIcon /><span className='text-blue'> Instagram</span></a></li>
-
                     <li className="text-yellow"><a href="mailto:varad.code19@gmail.com"> <EmailIcon /> <span className="text-blue"> Email </span> </a></li>
+                    <br />
+                    <li className="text-yellow">Discord<span className='text-blue'> fasterelm</span></li>
                 </ol>
             </div>
             <br />
