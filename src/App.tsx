bb1022f23263f6c495f4c3b4e4c6d62a38fb1976
Varad Sharma <varad.code19@gmail.com>
2023-12-08 18:31:03 +0530

    Neovide support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
-	<h1>Work under progress. The new version will be pushed soon</h1>
-    </>
+    <div>
+      <h1>Work under progress. The new version will be pushed soon</h1>
+    </div>
   )
 }
 
