8a2b20a7b78f45d08f5a8e464b479c0f596e60e5
Varad Sharma <varad.code19@gmail.com>
2023-12-12 16:37:52 +0530

    About section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,10 @@ const Hero = () => {
     <div className="hero md:mt-[20vh] flex flex-col md:flex-row justify-between">
       <div className="mt-8 md:ml-8">
         <h1 className="text-[5.5rem] md:text-9xl p-2 pr-0 md:p-5 ml-2 md:ml-5 font-bold">Varad <br /> Sharma</h1>
-        <p className="text-slate-600 pl-4 ml-1 md:pl-5 md:ml-6 mb-5">FULLSTACK WEB DEVELOPER</p>
+        <p className="text-slate-600 pl-4 ml-1 md:pl-5 md:ml-6 mb-5 uppercase">WEB DEVELOPER <br /> mimialist ui</p>
       </div>
       <div className="-mt-12">
-        <img src="/logo-trans.png" alt="Varad Sharma" className="float-right md:ml-[45vw]"/>
+        <img src="/logo-trans.png" alt="Varad Sharma" className="float-right md:ml-[45vw]" />
       </div>
     </div>
   );
