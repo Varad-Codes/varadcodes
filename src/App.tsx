041e0e9bf9a3c801f71acd4c860267df41738839
Varad Sharma <varad.code19@gmail.com>
2023-12-13 19:42:21 +0530

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <Nav />
       </div>
       <div className="flex flex-col items-center content-center">
-        <div className="max-w-8xl ">
+        <div className="max-w-8xl">
           <Hero />
           <About />
         </div>
