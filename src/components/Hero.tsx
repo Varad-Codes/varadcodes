f1326ccc40d4a02210b0494e4542308a46a16b66
Varad Sharma <varad.code19@gmail.com>
2023-12-20 17:33:57 +0530

    new thingy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,19 @@
+import { motion } from 'framer-motion';
+
 const Hero = () => {
   return (
-    <div className="hero md:mt-[20vh] flex flex-col md:flex-row justify-between">
-      <div className="mt-8 md:ml-8">
+    <div className="hero bg-hero-pattern  rounded-xl text-center p-5 md:mt-[20vh] md:m-12 flex flex-col md:flex-row justify-center">
+      <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        className="bg-[#00000b33] rounded-xl backdrop-blur-md mt-8 md:ml-8"
+      >
         <h1 className="text-[5.5rem] md:text-9xl p-2 pr-0 md:p-5 ml-2 md:ml-5 font-bold">Varad <br /> Sharma</h1>
-        <p className="text-slate-600 pl-4 ml-1 md:pl-5 md:ml-6 mb-5 uppercase">WEB DEVELOPER <br /> mimialist ui</p>
-      </div>
-      <div className="-mt-12">
-        <img src="/logo-trans.png" alt="Varad Sharma" className="float-right md:ml-[45vw]" />
-      </div>
+        <p className="text-slate-600 pl-4 ml-1 md:pl-5 md:ml-6 mb-5 uppercase">WEB DEVELOPER <br /> mimialist    ui</p>
+      </motion.div>
+      {/* <div className="-mt-12"> */}
+      {/*   <img src="/logo-trans.png" alt="Varad Sharma" className="float-right md:ml-[45vw]" /> */}
+      {/* </div> */}
     </div>
   );
 };
