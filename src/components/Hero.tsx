559610de09ff4ae7f0b7e652a1d0e59e7dfc7ccf
Varad Sharma <varad.code19@gmail.com>
2024-01-01 16:34:26 +0530

    Hero repositioning

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,12 +10,12 @@ const Hero = () => {
   // });
 
   return (
-    <div className="hero rounded-xl text-center p-5 md:mt-[20vh] md:m-12 flex flex-col justify-center">
-      <div className="hero-bg flex justify-center h-screen w-screen -z-10 absolute" ></div>
+    <div className="hero rounded-xl text-center p-5 md:mt-[15vh] md:m-10 flex flex-col justify-center">
+      <div className="hero-bg flex content-center justify-center h-[50vh] md:h-[90vh] w-screen md:w-[90vw] -z-10 absolute" ></div>
       <motion.div 
         initial={{opacity: 0}}
         whileInView={{opacity: 1}}
-        className="rounded-xl mt-8 md:ml-8"
+        className="mt-8 md:ml-8"
       >
         <h1 className="text-[5.5rem] md:text-9xl p-2 pr-0 md:p-5 ml-2 md:ml-5 font-bold">Varad <br /> Sharma</h1>
         <p className="text-slate-500 pl-4 ml-1 md:pl-5 md:ml-6 mb-5 uppercase">WEB DEVELOPER <br /> mimialist    ui</p>
