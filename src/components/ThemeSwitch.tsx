624e2fbca983f1204dbed4a63da3c82ad876697f
Varad Sharma <varad.code19@gmail.com>
2024-01-04 15:40:49 +0530

    Cleanup

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -6,30 +6,22 @@ import { useState } from 'react';
 const ThemeSwitch = () => {
   const [darkMode, setDarkMode] = useState(true);
 
-  // useEffect(() => {
-  //   const m = window.matchMedia("(prefers-color-scheme: dark)");
-  //   setDarkMode(m.matches); // Update darkMode only once based on media query
-  // }, []); // Empty dependency array to run only once
-
   const handleThemeChange = () => {
     setDarkMode(!darkMode); // Toggle darkMode state
-    console.log("line 18", darkMode);
     const main = document.getElementById("root")
 
     if (!darkMode) {
       main?.classList.remove("light")
       main?.classList.add("dark")
-    }
-    else {
+    } else {
       main?.classList.remove("dark")
       main?.classList.add("light")
     }
-    // document.getElementById("main")?.classList.add()
   };
 
   return (
     <div>
-      <button onClick={handleThemeChange} className={darkMode ? "": "invert"}>
+      <button onClick={handleThemeChange} className={darkMode ? "" : "invert"}>
         {darkMode ? <DarkModeIcon /> : <LightModeIcon />}
       </button>
     </div>
