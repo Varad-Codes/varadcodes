2ba84004dc09c78975acb40bbe230946a3a34ac8
Varad Sharma <varad.code19@gmail.com>
2024-01-05 21:51:40 +0530

    Responsive design update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,53 +1,58 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 const Hero = () => {
-
   return (
-    <div className="text-center h-screen w-screen p-5 flex flex-col justify-center content-center">
+    <div className="text-center  w-[90vw] flex flex-col justify-center content-center">
       {/* <div className="hero-bg flex content-center justify-center h-[50vh] md:h-[90vh] w-screen md:w-[90vw] -z-10 absolute" ></div> */}
       {/* Desktop */}
-      <div className='hero-cnt justify-center content-center hidden md:grid'>
-        <div className='hero-cnt-ver'>
-
+      <div className="hero-cnt justify-center content-center hidden md:grid">
+        <div className="hero-cnt-ver">
           {/* Vertical#1 */}
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             className="mt-8  hero-bento-el"
           >
-            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Skills</h1>
+            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+              Skills
+            </h1>
           </motion.div>
 
-
           {/* Vertical#2 */}
-          <div className='flex gap-6 flex-col md:flex-row'> {/* Horizontal */}
-
+          <div className="flex gap-6 flex-col md:flex-row">
+            {" "}
+            {/* Horizontal */}
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               className="mt-8  hero-bento-el"
             >
-              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Education</h1>
+              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+                Experience
+              </h1>
             </motion.div>
-
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               className="mt-8  hero-bento-el"
             >
-              <h1 className="text-[5rem] md:text-8xl p-2 pr-0 md:p-5 font-bold ">Varad <br /> Sharma</h1>
-              <p className="text-slate-500 tracking-[0.55rem] mb-5 uppercase">WEB DEVELOPER <br /> mimialist    ui</p>
+              <h1 className="text-[5rem] md:text-8xl p-2 pr-0 md:p-5 font-bold ">
+                Varad <br /> Sharma
+              </h1>
+              <p className="text-slate-500 tracking-[0.55rem] mb-5 uppercase">
+                WEB DEVELOPER <br /> mimialist ui
+              </p>
             </motion.div>
-
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               className="mt-8  hero-bento-el"
             >
-              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Projects</h1>
+              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+                Projects
+              </h1>
             </motion.div>
           </div>
-
 
           {/* Vertical#3 */}
           <motion.div
@@ -55,22 +60,27 @@ const Hero = () => {
             whileInView={{ opacity: 1 }}
             className="mt-8  hero-bento-el"
           >
-            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 ml-bold">Expericence</h1>
+            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 ml-bold">
+              Education
+            </h1>
           </motion.div>
         </div>
       </div>
 
       {/* Mobile */}
-      <div className='hero-cnt justify-center content-center grid mt-4 md:hidden'>
-        <div className='hero-cnt-ver'>
-
+      <div className="hero-cnt justify-center content-center grid mt-4 md:hidden">
+        <div className="hero-cnt-ver">
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             className="mt-8 p-4 hero-bento-el"
           >
-            <h1 className="text-[5rem] md:text-8xl p-2 pr-0 md:p-5 font-bold">Varad <br /> Sharma</h1>
-            <p className="text-slate-500 tracking-[0.55rem] mb-5 uppercase">WEB DEVELOPER <br /> mimialist    ui</p>
+            <h1 className="text-[5rem] md:text-8xl p-2 pr-0 md:p-5 font-bold">
+              Varad <br /> Sharma
+            </h1>
+            <p className="text-slate-500 tracking-[0.55rem] mb-5 uppercase">
+              WEB DEVELOPER <br /> mimialist ui
+            </p>
           </motion.div>
 
           {/* Vertical#2 */}
@@ -79,29 +89,33 @@ const Hero = () => {
             whileInView={{ opacity: 1 }}
             className="mt-8  hero-bento-el"
           >
-            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Skills</h1>
+            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+              Skills
+            </h1>
           </motion.div>
 
-          <div className='flex gap-6 flex-col md:flex-row'> {/* Horizontal */}
-
+          <div className="flex gap-6 flex-col md:flex-row">
+            {" "}
+            {/* Horizontal */}
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               className="mt-8  hero-bento-el"
             >
-              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Education</h1>
+              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+                Experience
+              </h1>
             </motion.div>
-
-
             <motion.div
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               className="mt-8  hero-bento-el"
             >
-              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">Projects</h1>
+              <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 font-bold">
+                Projects
+              </h1>
             </motion.div>
           </div>
-
 
           {/* Vertical#3 */}
           <motion.div
@@ -109,7 +123,9 @@ const Hero = () => {
             whileInView={{ opacity: 1 }}
             className="mt-8  hero-bento-el"
           >
-            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 ml-bold">Expericence</h1>
+            <h1 className="text-[3.2rem] md:text-4xl p-2 pr-0 md:p-5 ml-bold">
+              Education
+            </h1>
           </motion.div>
         </div>
       </div>
